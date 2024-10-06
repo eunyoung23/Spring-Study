9ad75b6be23fef5333c3552ce874f40db396b355
eun02323 <eun02323@naver.com>
2024-10-06 22:43:24 +0900

    add filterintercepter

---
+++ filterInterceptor/src/test/java/com/example/filterInterceptor/FilterInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filterInterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
