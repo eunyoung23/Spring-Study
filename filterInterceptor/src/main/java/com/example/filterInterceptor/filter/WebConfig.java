bafb4f550d542c24bfc8d24477154bcdb140732d
eun02323 <eun02323@naver.com>
2024-10-09 18:27:15 +0900

    add interceptor

--- filterInterceptor/src/main/java/com/example/filterInterceptor/filter/WebConfig.java
+++ filterInterceptor/src/main/java/com/example/filterInterceptor/filter/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
+
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
